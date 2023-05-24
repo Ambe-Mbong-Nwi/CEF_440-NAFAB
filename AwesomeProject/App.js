868ba3bece1bcda447b545fb8aefa20eaf4e2c69
Blade <nkwetakembrunodudu9@gmail.com>
2023-05-24 22:17:25 +0100

    changes done

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -15,7 +15,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop:100,
+    padding:10
   },
 });
--- AwesomeProject/Componetns/Form/SignUp.jsx
+++ AwesomeProject/Componetns/Form/SignUp.jsx
@@ -1,17 +1,21 @@
 import React, { Component } from 'react'
-import { Text, View ,TextInput} from 'react-native'
+import { Text, View ,TextInput, Picker, Button,StyleSheet} from 'react-native'
 
 export class SignUp extends Component {
   render() {
     return (
       <View>
 
-        <Text>
+        <Text
+          style={{
+          marginBottom:10,
+          fontSize:20
+           }}
+        >
        Sign Up with
        <Text 
                style={{
                 color:'#0CAA00',
-                fontWeight:'900',
               }}
        >  Carryam GO</Text> 
       </Text>
@@ -19,33 +23,72 @@ export class SignUp extends Component {
       <View>
       <TextInput
               style={{
-                height: 40,
+                height: 50,
                 borderColor: 'gray',
                 borderWidth: 1,
+                marginTop:8,
+                marginBottom:10,
+                paddingLeft:7,
+                borderRadius:7
 
               }}
-      defaultValue='Username'
+              placeholder='Username'
       />
           <TextInput
                   style={{
-                    height: 40,
+                    height: 50,
                     borderColor: 'gray',
                     borderWidth: 1,
+                    marginTop:8,
+                    marginBottom:10,
+                    paddingLeft:7,
+                    borderRadius:7
                   }}
-      defaultValue='Email'
+                  placeholder='Email'
       />
           <TextInput
                   style={{
-                    height: 40,
+                    height: 50,
                     borderColor: 'gray',
                     borderWidth: 1,
+                    marginTop:5,
+                    marginTop:8,
+                    marginBottom:10,
+                    borderRadius:7,
+                    paddingLeft:7,
                   }}
-      defaultValue='Phone number'
+    placeholder='Phone number'
       />
+  {/* subjected to changes for the select input field  */}
+  <TextInput
+                  style={{
+                    height: 50,
+                    borderColor: 'gray',
+                    borderWidth: 1,
+                    marginTop:5,
+                    marginTop:8,
+                    marginBottom:10,
+                    borderRadius:7,
+                    paddingLeft:7,
+                  }}
+     secureTextEntry={true}
+     placeholder='password'
+      />
+
+    <Button
+     title='Sign ps'
+     style={{
+        backgroundColor: "red",
+        width: 10,
+        height: 40,
+        borderRadius: 50,
+        color:'red'
+      }} />
       </View>
       </View>
     )
   }
+
 }
 
 export default SignUp
