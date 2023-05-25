aca15111f8c6b8b74e01a955d75aaf362046b5d9
Blade <nkwetakembrunodudu9@gmail.com>
2023-05-25 11:12:47 +0100

    inline styles abolished

--- AwesomeProject/Componetns/Form/SignUp.jsx
+++ AwesomeProject/Componetns/Form/SignUp.jsx
@@ -1,94 +1,69 @@
-import React, { Component } from 'react'
-import { Text, View ,TextInput, Picker, Button,StyleSheet} from 'react-native'
+import { View, Text,TextInput,Button,StyleSheet } from 'react-native'
+import React from 'react'
 
-export class SignUp extends Component {
-  render() {
-    return (
-      <View>
+export default function SignUp() {
+  return (
+    <View>
 
-        <Text
-          style={{
-          marginBottom:10,
-          fontSize:20
-           }}
-        >
-       Sign Up with
-       <Text 
-               style={{
-                color:'#0CAA00',
-              }}
-       >  Carryam GO</Text> 
-      </Text>
+    <Text
+      style={{
+      marginBottom:10,
+      fontSize:20
+       }}
+    >
+   Sign Up with
+   <Text 
+           style={{
+            color:'#0CAA00',
+          }}
+   >  Carryam GO</Text> 
+  </Text>
 
-      <View>
-      <TextInput
-              style={{
-                height: 50,
-                borderColor: 'gray',
-                borderWidth: 1,
-                marginTop:8,
-                marginBottom:10,
-                paddingLeft:7,
-                borderRadius:7
-
-              }}
-              placeholder='Username'
-      />
-          <TextInput
-                  style={{
-                    height: 50,
-                    borderColor: 'gray',
-                    borderWidth: 1,
-                    marginTop:8,
-                    marginBottom:10,
-                    paddingLeft:7,
-                    borderRadius:7
-                  }}
-                  placeholder='Email'
-      />
-          <TextInput
-                  style={{
-                    height: 50,
-                    borderColor: 'gray',
-                    borderWidth: 1,
-                    marginTop:5,
-                    marginTop:8,
-                    marginBottom:10,
-                    borderRadius:7,
-                    paddingLeft:7,
-                  }}
-    placeholder='Phone number'
-      />
-  {/* subjected to changes for the select input field  */}
+  <View>
   <TextInput
-                  style={{
-                    height: 50,
-                    borderColor: 'gray',
-                    borderWidth: 1,
-                    marginTop:5,
-                    marginTop:8,
-                    marginBottom:10,
-                    borderRadius:7,
-                    paddingLeft:7,
-                  }}
-     secureTextEntry={true}
-     placeholder='password'
-      />
+  style={styles.Textinput}
+          placeholder='Username'
+  />
+      <TextInput
+            style={styles.Textinput}
+              placeholder='Email'
+  />
+      <TextInput
+style={styles.Textinput}
+placeholder='Phone number'
+  />
+{/* subjected to changes for the select input field  */}
+<TextInput
+style={styles.Textinput}
+ secureTextEntry={true}
+ placeholder='password'
+  />
 
-    <Button
-     title='Sign ps'
-     style={{
-        backgroundColor: "red",
-        width: 10,
-        height: 40,
-        borderRadius: 50,
-        color:'red'
-      }} />
-      </View>
-      </View>
-    )
-  }
+<Button
+ title='Sign up'
+ style={{
+    backgroundColor: "red",
+    width: 10,
+    height: 40,
+    borderRadius: 50,
+    color:'red'
+  }} />
 
+  </View>
+  </View>
+  )
 }
 
-export default SignUp
+// write the styles for the App 
+const styles= StyleSheet.create({
+    Textinput:{
+        height: 50,
+        borderColor: 'gray',
+        borderWidth: 1,
+        marginTop:5,
+        marginTop:8,
+        marginBottom:10,
+        borderRadius:7,
+        paddingLeft:7  
+    }
+})
