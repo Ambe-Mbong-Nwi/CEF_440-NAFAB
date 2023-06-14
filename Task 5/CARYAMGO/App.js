ef2349c8ef00d8feff2d50f51e8abe83c9a245ce
BLADE-HEDA <you@example.com>
2023-06-14 13:30:39 +0100

    changes done to the unsplash screen

--- Task 5/CARYAMGO/App.js
+++ Task 5/CARYAMGO/App.js
@@ -1,13 +1,9 @@
 import { useCallback, useEffect, useState } from 'react';
-import { Button, StyleSheet, Text, View, ScrollView } from 'react-native';
-import {NavigationContainer, useNavigation} from '@react-navigation/native';
-// import { createStackNavigator } from '@react-navigation/stack';
+import { Text, View } from 'react-native';
 import Entypo from '@expo/vector-icons/Entypo';
 import * as SplashScreen from 'expo-splash-screen';
 import * as Font from 'expo-font';
-// import the modules for the App 
-import { StatusBar } from 'expo-status-bar';
-//import LoginForm from './Componetns/Shared/Butoon';
+// imoirt the various components 
 import WelcomePage from './pages/WelcomePage';
 import AddToCart from './pages/AddToCart';
 import Notification from './pages/Notification';
@@ -21,11 +17,13 @@ import VerificationNum from './pages/VerificationNum';
 import VerificationEmail from './pages/VerificationEmail';
 import ProfilePage from './pages/ProfilePage';
 
-// Keep the splash screen visible while we fetch resources to be displayed
+
+// Keep the splash screen visible while we fetch resources
 SplashScreen.preventAutoHideAsync();
 
 export default function App() {
   const [appIsReady, setAppIsReady] = useState(false);
+
   useEffect(() => {
     async function prepare() {
       try {
@@ -33,7 +31,7 @@ export default function App() {
         await Font.loadAsync(Entypo.font);
         // Artificially delay for two seconds to simulate a slow loading
         // experience. Please remove this if you copy and paste the code!
-        await new Promise(resolve => setTimeout(resolve, 1000));
+        await new Promise(resolve => setTimeout(resolve, 2000));
       } catch (e) {
         console.warn(e);
       } finally {
@@ -52,7 +50,6 @@ export default function App() {
       // loading its initial state and rendering its first pixels. So instead,
       // we hide the splash screen once we know the root view has already
       // performed layout.
-
       await SplashScreen.hideAsync();
     }
   }, [appIsReady]);
@@ -60,17 +57,12 @@ export default function App() {
   if (!appIsReady) {
     return null;
   }
-//  subjected to changes
-// const Stack = createStackNavigator();
+
   return (
-    <NavigationContainer> 
     <View
       style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
-      onLayout={onLayoutRootView}
-      >
-   <OptionPage/>
-  
+      onLayout={onLayoutRootView}>
+     <OptionPage/>
     </View>
-    </NavigationContainer> 
   );
 }
