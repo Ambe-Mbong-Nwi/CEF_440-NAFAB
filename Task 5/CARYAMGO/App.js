a6ea2af0e7d4d349622e6d9fc7db8513cfac6da1
Nkwetakem Bruno <nkwetakembrunodudu9@gmail.com>
2023-06-14 13:01:57 +0100

    Add files via upload

--- Task 5/CARYAMGO/App.js
+++ Task 5/CARYAMGO/App.js
@@ -1,7 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
+import { useCallback, useEffect, useState } from 'react';
 import { Button, StyleSheet, Text, View, ScrollView } from 'react-native';
-import { useFonts } from 'expo-font';
-
+import {NavigationContainer, useNavigation} from '@react-navigation/native';
+// import { createStackNavigator } from '@react-navigation/stack';
+import Entypo from '@expo/vector-icons/Entypo';
+import * as SplashScreen from 'expo-splash-screen';
+import * as Font from 'expo-font';
+// import the modules for the App 
+import { StatusBar } from 'expo-status-bar';
 //import LoginForm from './Componetns/Shared/Butoon';
 import WelcomePage from './pages/WelcomePage';
 import AddToCart from './pages/AddToCart';
@@ -15,52 +20,57 @@ import ForgotPassword from './pages/ForgotPassword';
 import VerificationNum from './pages/VerificationNum';
 import VerificationEmail from './pages/VerificationEmail';
 import ProfilePage from './pages/ProfilePage';
-import Navigation from './routes/Navigation';
-import { NavigationContainer } from '@react-navigation/native';
-import Subscription from './pages/Subscription';
-// import { createStackNavigator } from '@react-navigation/stack';
 
-// const Stack = createStackNavigator();
-
+// Keep the splash screen visible while we fetch resources to be displayed
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
-  return (
-  // // <View > 
+  const [appIsReady, setAppIsReady] = useState(false);
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // Pre-load fonts, make any API calls you need to do here
+        await Font.loadAsync(Entypo.font);
+        // Artificially delay for two seconds to simulate a slow loading
+        // experience. Please remove this if you copy and paste the code!
+        await new Promise(resolve => setTimeout(resolve, 1000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setAppIsReady(true);
+      }
+    }
 
-  // //        <Subscription />
-  //       {/* <Notification /> */}
-  //        {/* <OptionPage/> */}
-  //  {/* <Intropage/> */}
-  //       {/* <SignUp/> */}
-  //      {/* <SignUpVendor/> */}
-  //       {/* <Login/> */}
-  //       {/* <ForgotPassword/> */}
-  //      {/* <VerificationNum/> */}
-  //      {/* <VerificationEmail/> */}
-  //       {/* <ProfilePage shopnmame="Leony shop" market =" Muea market"
-  // //     // Email="loenglain@gmail.com" Phonenumber="674733043" profilestatus="Buyer"
-  // //     // location="Buea" marketname="Central Market" shadename="Shade 5" password="*************"
-  // //     // /> */}
-  //       {/* <AddToCart /> */}
-  //      {/* {/* // // <WelcomePage /> */}
-      <NavigationContainer>
-           <Navigation />
-      </NavigationContainer> 
-      // </View> 
-    
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      // This tells the splash screen to hide immediately! If we call this after
+      // `setAppIsReady`, then we may see a blank screen while the app is
+      // loading its initial state and rendering its first pixels. So instead,
+      // we hide the splash screen once we know the root view has already
+      // performed layout.
+
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+//  subjected to changes
+// const Stack = createStackNavigator();
+  return (
+    <NavigationContainer> 
+    <View
+      style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
+      onLayout={onLayoutRootView}
+      >
+   <OptionPage/>
+  
+    </View>
+    </NavigationContainer> 
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    },
-    content: {
-      flex: 1,       //dont go off the screen just ensure content expands to fill the screen.
-      padding: 5,           //move it away from top of screen
-    },
-    
-  });
