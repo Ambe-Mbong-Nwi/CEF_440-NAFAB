9f09a4a6ab2097646d02a2fb5464a2833da6b844
BLADE-HEDA <you@example.com>
2023-06-17 10:54:00 +0100

    changes remade and cedits left

--- Task 5/CARYAMGO/App.js
+++ Task 5/CARYAMGO/App.js
@@ -26,7 +26,6 @@ export default function App() {
       {/* <NavugationVendor/> */}
       {/* <NavugationBuyer/> */}
       <Navigation1/>
-      {/* <Card1/> */}
       {/* <Navigation/> */}
       {/* <Navigation1/> */}
     {/* <Stack.Navigator>
